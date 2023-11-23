6af005cac1d983a18da605774e9f2e237008c1d2
Fair Lucas <lukitaa.27@gmail.com>
2023-11-23 13:44:25 -0300

    register design

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './presentation/pages/register/register.component';
 
 export const routes: Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
     { 
         path: '**', 
         pathMatch: 'full',
