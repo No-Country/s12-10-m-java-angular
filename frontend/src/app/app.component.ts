3fbb0d221e98fb074edd3385de7228f0140c520a
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2023-11-28 16:34:58 -0300

    [ADD] Footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./presentation/components/navbar/navbar.component";
+import { FooterComponent } from "./presentation/components/footer/footer.component";
 import { LoginComponent } from './presentation/pages/login/login.component';
 import { RegisterComponent } from './presentation/pages/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,13 +13,12 @@ import { CatalogComponent } from "./presentation/pages/catalog/catalog.component
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RegisterComponent, LoginComponent, CatalogComponent, NavbarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [ToastService]
-
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    providers: [ToastService],
+    imports: [CommonModule, RouterOutlet, RegisterComponent, LoginComponent, CatalogComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent implements OnInit {
   
