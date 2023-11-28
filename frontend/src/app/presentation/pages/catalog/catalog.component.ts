1a3b2189f7ee722117e2caad2c85d98dd4a341a7
Agustin Rios <agustin528rios@gmail.com>
2023-11-28 12:09:22 -0300

    Update catalog.component.ts

--- frontend/src/app/presentation/pages/catalog/catalog.component.ts
+++ frontend/src/app/presentation/pages/catalog/catalog.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Book } from '../../../data/models/book';
 import { CardBookComponent } from '../../components/card-book/card-book.component';
-import { booksArray } from '../../../data/mocks/books';
+import { booksArray } from '../../../data/mocks/booksArray';
 
 @Component({
   selector: 'app-catalog',
