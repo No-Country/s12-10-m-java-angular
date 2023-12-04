91e4f0458260906466d3787e3dcb3c10d2c765bb
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2023-12-04 12:43:54 -0300

    [FIX] navbar component separation

---
+++ frontend/src/app/presentation/components/navbar-2/navbar-2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar-2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar-2.component.html',
+  styleUrl: './navbar-2.component.css'
+})
+export class Navbar2Component {
+
+}
