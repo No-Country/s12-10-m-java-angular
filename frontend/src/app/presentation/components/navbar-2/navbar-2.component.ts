6613eb427422b07ceecb94b2a26351b85b86d426
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2023-12-04 14:50:17 -0300

    [ADD] navbar-2

--- frontend/src/app/presentation/components/navbar-2/navbar-2.component.ts
+++ frontend/src/app/presentation/components/navbar-2/navbar-2.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-2',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar-2.component.html',
   styleUrl: './navbar-2.component.css'
 })
