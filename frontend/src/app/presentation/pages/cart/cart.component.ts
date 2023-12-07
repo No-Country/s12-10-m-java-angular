3adbb02a1ca5ea29db6a39cf1fef81af7296f2df
Agustin Rios <agustin528rios@gmail.com>
2023-12-07 10:26:24 -0300

    remove book of cart

--- frontend/src/app/presentation/pages/cart/cart.component.ts
+++ frontend/src/app/presentation/pages/cart/cart.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CardBookComponent } from '@presentation/components/card-book/card-book.component';
-import { BOOK_DETAIL_MOOK } from 'app/data/mocks/booksArray';
 import { BookDetail } from 'app/data/models/book';
 import { Observable, delay, of } from 'rxjs';
 import { FooterComponent } from '@presentation/components/footer/footer.component';
 import { Navbar2Component } from '@presentation/components/navbar-2/navbar-2.component';
 import { NavbarComponent } from '@presentation/components/navbar/navbar.component';
 import { CartCardComponent } from '@presentation/components/cart-card/cart-card.component';
+import { CartService } from 'app/data/services/cart/cart.service';
 
 @Component({
   standalone: true,
@@ -19,17 +19,19 @@ import { CartCardComponent } from '@presentation/components/cart-card/cart-card.
 })
 export class CartComponent implements OnInit {
 
-  protected books: Observable<BookDetail[]>;
+  protected booksWQ: { book: BookDetail, quantity: number }[];
 
-  constructor() {
-    this.books = new Observable();
+  constructor(public cartService: CartService) {
+    this.booksWQ = cartService.bringCartOfServiceWithQuantity();
+    console.log(this.booksWQ)
    }
 
   ngOnInit(): void {
+  }
 
-    this.books = of(BOOK_DETAIL_MOOK).pipe(
-      delay(1000)
-    );
+  discardBook(bookToRemove: BookDetail){
+    this.booksWQ = this.booksWQ.filter(bookWithQuantity => bookWithQuantity.book.ID !== bookToRemove.ID);
+    this.cartService.deleteBookToCart(bookToRemove)
   }
 
 }
