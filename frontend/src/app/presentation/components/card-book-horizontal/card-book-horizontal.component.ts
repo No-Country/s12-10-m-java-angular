983212a02a9cc2a03c64cfe7b624de91c92ac57c
Agustin Rios <agustin528rios@gmail.com>
2023-12-07 10:24:13 -0300

    add/remove book of horizontal card

--- frontend/src/app/presentation/components/card-book-horizontal/card-book-horizontal.component.ts
+++ frontend/src/app/presentation/components/card-book-horizontal/card-book-horizontal.component.ts
@@ -1,26 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Book, BookDetail } from '../../../data/models/book';
 import { RouterLink } from '@angular/router';
-// import { DefaultButtonComponent } from '../default-button/default-button.component'; , DefaultButtonComponent esto abajo en imports
+import { DefaultButtonComponent } from '../default-button/default-button.component';
+import { CartService } from 'app/data/services/cart/cart.service';
 
 @Component({
   selector: 'app-card-book-horizontal',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, RouterLink],
+  imports: [CommonModule, NgOptimizedImage, RouterLink, DefaultButtonComponent],
   templateUrl: './card-book-horizontal.component.html',
-  styleUrl: './card-book-horizontal.component.css'
+  styleUrls: ['./card-book-horizontal.component.css', '../default-button/default-button.component.css']
 })
 export class CardBookHorizontalComponent {
   @Input() public book: BookDetail;
   @Input() public className: string = "";
-  @Input() public horizontalCard: boolean = false;
+  @Input() public onCart: boolean = false;
 
-  constructor(){
+  constructor(public cartService: CartService) {
     this.book = {} as BookDetail;
   }
 
+  addOrRemove() {
+    this.onCart ? this.cartService.deleteBookToCart(this.book): this.cartService.addBookToCart(this.book);
+    this.onCart = !this.onCart;
+  }
+
+  removeFromCart(){
+    this.book
+  }
+
+  addToCart(){
+
+  }
+
   isNumberId(ID: string | number) {
-    return typeof ID === 'string'; 
+    return typeof ID === 'string';
   }
 }
