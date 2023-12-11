16709ccfa5dc38f29115bef60ef2b3fdfe0b461c
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2023-12-11 14:58:20 -0300

    [EDIT] Navbar-2 & dropdown

--- frontend/src/app/presentation/components/navbar-2/navbar-2.component.ts
+++ frontend/src/app/presentation/components/navbar-2/navbar-2.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { BookDetail } from 'app/data/models/book';
+import { MobileDropdownNavComponent } from "../mobile-dropdown-nav/mobile-dropdown-nav.component";
 
 @Component({
-  selector: 'app-navbar-2',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './navbar-2.component.html',
-  styleUrl: './navbar-2.component.css'
+    selector: 'app-navbar-2',
+    standalone: true,
+    templateUrl: './navbar-2.component.html',
+    styleUrl: './navbar-2.component.css',
+    imports: [CommonModule, RouterLink, MobileDropdownNavComponent]
 })
 export class Navbar2Component implements OnInit {
+
+  dropdownMenu = false;
 
   constructor(
     private router:Router
@@ -23,5 +25,9 @@ export class Navbar2Component implements OnInit {
   search(q:any){
     console.log(q);
     // this.router.navigate("shop/+ ${q}]")
+  }
+
+  showDropdown() {
+    this.dropdownMenu = !this.dropdownMenu
   }
 }
