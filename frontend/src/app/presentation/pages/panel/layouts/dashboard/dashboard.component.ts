690ea25fdabc8711f9c288a95447ddf8dae71fa7
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2023-12-18 16:28:50 -0300

    [ADD] Dashboard

--- frontend/src/app/presentation/pages/panel/layouts/dashboard/dashboard.component.ts
+++ frontend/src/app/presentation/pages/panel/layouts/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { PageTabComponent } from "../../../../components/admin/page-tab/page-tab.component";
+import { AdminCardComponent } from "../../../../components/admin/admin-card/admin-card.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        PageTabComponent,
+        AdminCardComponent
+    ]
 })
 export class DashboardComponent implements OnInit {
 
