c28971065734e7f556bfee8d429d8217779a0332
Fair Lucas <lukitaa.27@gmail.com>
2023-12-20 18:55:38 -0300

    width img dash

---
+++ frontend/src/app/data/services/register/suscribe.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Injector } from '@angular/core';
+import { ApiService } from '../api.service';
+import { AuthResponse } from 'app/data/models/AuthResponse';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuscribeService {
+
+  private api: ApiService = this.injector.get(ApiService)
+
+  constructor(private injector: Injector) { }
+
+  public suscribe(auth: AuthResponse): Observable<any>{
+    return this.api.httpPost('user/subscribe/${email}', auth, true);
+  }
+
+}
--- frontend/src/app/presentation/pages/home/layouts/news-letter/news-letter.component.ts
+++ frontend/src/app/presentation/pages/home/layouts/news-letter/news-letter.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DefaultButtonComponent } from '@presentation/components/default-button/default-button.component';
+import { AuthResponse } from 'app/data/models/AuthResponse';
+import { SuscribeService } from 'app/data/services/register/suscribe.service';
 
 @Component({
   standalone: true,
@@ -11,10 +14,30 @@ import { DefaultButtonComponent } from '@presentation/components/default-button/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewsLetterComponent implements OnInit {
+  newsletterForm: FormGroup;
 
-  constructor() { }
+  constructor(private suscribeService: SuscribeService, private fb: FormBuilder) {
+    this.newsletterForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  subscribe() {
+    if (this.newsletterForm.valid) {
+      const email = this.newsletterForm.get('email')!.value;
+
+      this.suscribeService.suscribe(email).subscribe(
+        response => {
+          console.log('Subscription successful', response);
+        },
+        error => {
+          console.error('Subscription failed', error);
+        }
+      );
+    }
   }
 
 }
